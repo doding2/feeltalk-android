65f00b8c07b217d23e92d13022d96aa03632a77f
doding2 <dragon041414@gmail.com>
2024-05-27 23:04:35 +0900

    Bug-fix: Mixpanel handle duplicate question sheet, not answer sheet

--- app/src/main/java/com/clonect/feeltalk/new_domain/usecase/mixpanel/SetInAnswerSheetMixpanelUseCase.kt
+++ app/src/main/java/com/clonect/feeltalk/new_domain/usecase/mixpanel/SetInAnswerSheetMixpanelUseCase.kt
@@ -5,7 +5,7 @@ import com.clonect.feeltalk.new_domain.repository.mixpanel.MixpanelRepository
 class SetInAnswerSheetMixpanelUseCase(
     private val mixpanelRepository: MixpanelRepository,
 ) {
-    suspend operator fun invoke(isInQuestion: Boolean) {
-        mixpanelRepository.setInQuestionPage(isInQuestion)
+    suspend operator fun invoke(isInAnswer: Boolean) {
+        mixpanelRepository.setInAnswerSheet(isInAnswer)
     }
 }
